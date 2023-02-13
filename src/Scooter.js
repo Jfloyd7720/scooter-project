bec1676a2fef986c2f5296458cbd141f1dfbe3d6
= <121040760+Jfloyd7720@users.noreply.github.com>
2023-02-13 09:44:40 +0000

    classes

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,44 @@
-class Scooter{
-  // scooter code here
+class Scooter {
+  static nextSerial = 1;
+  constructor() {
+    this.station = null;
+    this.user = null;
+    this.serial = Scooter.nextSerial++;
+    this.charge = 100;
+    this.isBroken = false;
+  }
+  rent(user) {
+    if (this.charge > 20 && !this.isBroken) {
+      this.station = null;
+      this.user = user;
+    } else {
+      throw new Error("Scooter needs to charge or needs repair");
+    }
+  }
+  dock(station) {
+    this.station = station;
+    this.user = null;
+  }
+  recharge() {
+    const timerId = setInterval(() => {
+      if (this.charge < 100) {
+        this.charge += 10;
+        console.log(`Scooter ${this.serial} is now ${this.charge}% charged`);
+      } else {
+        clearInterval(timerId);
+      }
+    }, 1000);
+  }
+  requestRepair() {
+    setTimeout(() => {
+      this.isBroken = false;
+      console.log(`Repair completed for scooter ${this.serial}`);
+    }, 5000);
+  }
 }
 
 
+
+
 module.exports = Scooter
+
--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -2,7 +2,109 @@ const User = require('./User')
 const Scooter = require('./Scooter')
 
 class ScooterApp {
-  // ScooterApp code here
+  constructor() {
+    this.stations = {
+      "Location1": [],
+      "Location2": [],
+      "Location3": []
+    };
+    this.registeredUsers = {};
+  }
+
+  registerUser(username, password, age) {
+    if (this.registeredUsers.hasOwnProperty(username)) {
+      throw new Error("User already registered");
+    }
+
+    if (age < 18) {
+      throw new Error("User must be 18 or older");
+    }
+
+    const user = new User(username, password, age);
+    this.registeredUsers[username] = user;
+    console.log(`User ${username} has been registered`);
+    return user;
+  }
+
+  loginUser(username, password) {
+    if (!this.registeredUsers.hasOwnProperty(username)) {
+      throw new Error("Username is incorrect");
+    }
+
+    const user = this.registeredUsers[username];
+    user.login(password);
+    console.log(`User ${username} has been logged in`);
+  }
+
+  logoutUser(username) {
+    if (!this.registeredUsers.hasOwnProperty(username)) {
+      throw new Error("No such user is logged in");
+    }
+
+    const user = this.registeredUsers[username];
+    user.logout();
+    console.log(`User ${username} has been logged out`);
+  }
+
+  createScooter(station) {
+    if (!this.stations.hasOwnProperty(station)) {
+      throw new Error("No such station");
+    }
+
+    const scooter = new Scooter(station);
+    this.stations[station].push(scooter);
+    console.log(`Created new scooter at ${station}`);
+    return scooter;
+  }
+
+  dockScooter(scooter, station) {
+    if (!this.stations.hasOwnProperty(station)) {
+      throw new Error("No such station");
+    }
+
+    if (scooter.station === station) {
+      throw new Error("Scooter already at station");
+    }
+
+    const index = this.stations[scooter.station].indexOf(scooter);
+    if (index !== -1) {
+      this.stations[scooter.station].splice(index, 1);
+    }
+
+    scooter.dock(station);
+    this.stations[station].push(scooter);
+    console.log("Scooter is docked");
+  }
+
+  rentScooter(scooter, user) {
+    if (scooter.user !== null) {
+      throw new Error("Scooter already rented");
+    }
+
+    const stationScooters = this.stations[scooter.station];
+    const index = stationScooters.indexOf(scooter);
+    if (index !== -1) {
+      stationScooters.splice(index, 1);
+      scooter.rent(user);
+      console.log("Scooter is rented");
+    } else {
+      throw new Error("Scooter not found");
+    }
+  }
+
+  print() {
+    console.log("Registered users:");
+    for (const username in this.registeredUsers) {
+      console.log(`- ${username}`);
+    }
+
+    console.log("Stations:");
+    for (const station in this.stations) {
+      console.log(`- ${station}: ${this.stations[station].length} scooter(s)`);
+    }
+  }
 }
 
+
 module.exports = ScooterApp
+
